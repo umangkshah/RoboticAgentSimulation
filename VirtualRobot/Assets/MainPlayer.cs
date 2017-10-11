b5752473bcb10445d42ac2a1d77e7673f5fe9cba
Umang Shah <uks160030@csgrads1.utdallas.edu>
2017-10-11 12:26:12 -0500

    unity script to fecth csv into list

---
+++ VirtualRobot/Assets/MainPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour {
+	public string filename;
+
+	// Use this for initialization
+	void Start () {
+		Console.WriteLine ("running");
+		Debug.Log (filename);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
