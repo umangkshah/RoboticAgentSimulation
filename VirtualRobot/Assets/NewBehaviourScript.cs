e1fd4269d098509dce963a678effe3895563440d
Umang Shah <uks160030@csgrads1.utdallas.edu>
2017-10-11 11:58:08 -0500

    begin unity porject

---
+++ VirtualRobot/Assets/NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Console.WriteLine ("CALL ");
+		List<string> listA = new List<string>();
+		using(var reader = new StreamReader(@"H:\Mutlimedia Systems Project\RoboticAgentSimulation\sample3d.csv"))
+		{
+			Console.WriteLine ("reading ");
+			//List<string> listB = new List<string>();
+			while (!reader.EndOfStream)
+			{
+				var line = reader.ReadLine();
+				var values = line.Split(',');
+				foreach (var v in values) {
+					listA.Add (v);
+				}
+			}
+		}
+		Console.WriteLine ("readind done ");
+		foreach (var i in listA)
+			Console.WriteLine (i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
